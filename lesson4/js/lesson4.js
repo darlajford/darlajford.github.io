5dbc20a79a2bbc4a59424a4542cce60cfd23dbfb
darlajford <for20010@byui.edu>
2020-05-16 08:57:24 -0500

    final date commit

--- lesson4/js/lesson4.js
+++ lesson4/js/lesson4.js
@@ -8,10 +8,71 @@ function toggleMenu() {
 
 //The following is for date display in footer
 
+let daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let d = new Date();
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+
+//let fulldate = dayname + ", " + monthname + " " + d.getDate() +", " + year;  (shows day month date year format)
+let fulldate = dayname + ", " + d.getDate() + " " + monthname + " " + year;
+
+document.getElementById("lastupdated").textContent = fulldate;
+console.log(fulldate);
+
+document.getElementById("year").textContent = year;
 
 
-var d = new Date(); 
-document.getElementById("demo").innerHTML = d.toDateString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
