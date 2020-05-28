2642f3fd882429d8991e88ce8e2189f513667109
darlajford <for20010@byui.edu>
2020-05-28 13:36:08 -0500

    lesson6 commit

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,15 +1,19 @@
 
-function windChills() {
-  var wind = parseFloat(document.getElementById("wind").textContent);
-  var temp = parseFloat(document.getElementById("temp").textContent);
-  var chill = "N/A"
-
-  if (wind > 3 && temp < 50) {
-      var s = Math.pow(wind, 0.16);
-      chill = (35.74 + (0.6215 * temp) - (35.75 * s) + (0.4275 * temp * s)).toPrecision(2);;
-
-
-  }
-
-  document.getElementById("chill").innerHTML = chill;
+function calc(){
+  let temp = parseFloat(document.getElementById("temp").textContent);
+  let wind = parseFloat(document.getElementById("wind").textContent);
+  let result = chill(temp,wind);
+  
+  document.getElementById("chill").innerHTML = result;
+  
 }
+function chill(temp,wind){
+  if (temp <= 50 && wind > 3){
+  let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind,0.16) + 0.4275 * temp * Math.pow(wind,0.16);
+   return parseInt(f); 
+  } 
+  else{
+      return f = "N/A";       
+  }
+}
+calc()
