6023b4a970529f5543abbd7727fd3a4d8e60534f
darlajford <for20010@byui.edu>
2020-06-16 12:28:18 -0500

    week9 commit

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -49,11 +49,46 @@ document.getElementById("year").textContent = year;
 //to display pancake message on fridays
 const banner = document.getElementById("eventMessage");
 
-if (daynames[d.getDay()] == "Friday" || daynames[d.getDay()] == "Saturday" ) {
+if (daynames[d.getDay()] == "Friday" || daynames[d.getDay()] == "Saturday") {
   banner.style.display = "block";
 } else {
   banner.style.display = "none";
 }
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++) {
+      let town = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let motto = document.createElement('p');
+      let year = document.createElement('p');
+      let population = document.createElement('p');
+      let rainfall = document.createElement('p');
+
+      h2.textContent = towns[i].name;
+      motto.textContent = towns[i].motto;
+      year.textContent = 'Year Founded: ' + towns[i].yearFounded;
+      population.textContent = 'Population: ' + towns[i].currentPopulation;
+      rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+
+      town.appendChild(h2);      
+      town.appendChild(motto);
+      town.appendChild(year);
+      town.appendChild(population);
+      town.appendChild(rainfall);
+
+      document.querySelector('div.towns').appendChild(town); 
+ 
+    }
+  });
 
 //to control font loading
 WebFont.load({
