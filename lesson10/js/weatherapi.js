bf8eafb07e631db566a7d340fa23ee5f98f07d9d
darlajford <for20010@byui.edu>
2020-06-22 20:48:55 -0500

    week10 commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,7 +2,7 @@ const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    // console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
@@ -18,10 +18,3 @@ fetch(apiURL)
 
 
 
-// fetch(requestURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (jsonObject) {
-//     console.table(jsonObject);
-//   });
