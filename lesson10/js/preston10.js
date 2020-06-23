d27bb0a2cd1f7e6158af9a7850805796432f1340
darlajford <for20010@byui.edu>
2020-06-23 10:52:59 -0500

    week10 commit

--- lesson10/js/preston10.js
+++ lesson10/js/preston10.js
@@ -55,6 +55,45 @@ if (daynames[d.getDay()] == "Friday" || daynames[d.getDay()] == "Saturday") {
   banner.style.display = "none";
 }
 
+// weather summary information from api
+const requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=a442aaa7bf1b55d1a3f7a66de5e262fe';
+fetch(requestURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    // console.log(jsObject);
+    document.getElementById('condition').textContent = jsObject.weather[0].main;
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('humid').textContent = jsObject.main.humidity;
+    document.getElementById('wind').textContent = jsObject.wind.speed;
+
+    //wind chill factor
+    //--------------------------------------
+    function chill() {
+      let temp = parseFloat(document.getElementById("current-temp").textContent);
+      let wind = parseFloat(document.getElementById("wind").textContent);
+
+      if (temp <= 50 && wind > 3) {
+        let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
+
+        document.getElementById("chill").innerHTML = parseInt(f) + '&#176';
+      } else {
+        document.getElementById("chill").innerHTML = "N/A";
+      }
+    }
+    chill()
+
+    //weather icons for 5 day forecast
+    // ---------------------------
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+    const desc = jsObject.weather[0].description; // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+
+  });
+
+
+
 //---------------------------------
 //to control font loading
 WebFont.load({
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,19 +2,33 @@ const requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&u
 fetch(requestURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    // console.log(jsObject);
+    console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('humid').textContent = jsObject.main.humidity;
+    document.getElementById('wind').textContent = jsObject.wind.speed;
+    document.getElementById('condition').textContent = jsObject.weather[0].main;
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    //wind chill factor
+    // --------------------------------------
+    function chill() {
+      let temp = parseFloat(document.getElementById("current-temp").textContent);
+      let wind = parseFloat(document.getElementById("wind").textContent);
+
+      if (temp <= 50 && wind > 3) {
+        let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
+
+        document.getElementById("chill").innerHTML = parseInt(f);
+      } else {
+        document.getElementById("chill").innerHTML = "N/A";
+      }
+    }
+    chill()
+    // -------------------------------------
+    //weather icons and description
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+    const desc = jsObject.weather[0].description; // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
 
   });
-
-
-
-
-
-
