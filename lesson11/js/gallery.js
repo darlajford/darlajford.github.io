26c5f833a550decbb7f31ce07f1dd5e9267503c8
darlajford <for20010@byui.edu>
2020-06-29 18:47:44 -0500

    week11 commit

--- lesson11/js/gallery.js
+++ lesson11/js/gallery.js
@@ -78,7 +78,7 @@ document.getElementById("currentdate").textContent = fulldate;
 console.log(fulldate);
 
 //to display current year next to copyright (although copyright year should remain the same)
-document.getElementById("year").textContent = year;
+// document.getElementById("year").textContent = year;
 //to display pancake message on fridays
 const banner = document.getElementById("eventMessage");
 
