f2544648a394dbda892b576b07eb3d649cde66fb
darlajford <for20010@byui.edu>
2020-07-08 16:57:02 -0500

    week12 commit

---
+++ lesson12/js/scoots.js
@@ -0,0 +1,43 @@
+//hamburger menu button
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+//The following is for date display in footer
+let daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let d = new Date();
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+
+let fulldate = dayname + ", " + d.getDate() + " " + monthname + " " + year;
+
+document.getElementById("currentdate").textContent = fulldate;
+console.log(fulldate);
+
