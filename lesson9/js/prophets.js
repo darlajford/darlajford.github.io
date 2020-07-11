f7db05c14f415b82e6830c63da7f319eafec0bdc
darlajford <for20010@byui.edu>
2020-07-10 22:08:38 -0500

    week12 commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
     //const utah = prophets.filter(prophet => (prophet.birthplace == 'Utah'));  //this line is in addition to const prophets line
 
