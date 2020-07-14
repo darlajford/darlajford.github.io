5c50d4e1b18cd0255e756a8dffe6b9d4f4944148
darlajford <for20010@byui.edu>
2020-07-14 08:36:06 -0500

    week13 commit

--- lesson12/js/scootsRent.js
+++ lesson12/js/scootsRent.js
@@ -20,3 +20,60 @@ function renderData() {
 }
 
 renderData();
+
+
+// ==========================================
+// my table was built with the following information found at 
+// https://stackoverflow.com/questions/50451836/how-to-display-json-data-using-just-javascript-from-a-local-json-file-or-from-a
+
+{/* <table id="myTable" border="2">
+
+</table> */}
+
+// {/* <script> */}
+
+// var data = [
+//     {
+//         "id": "bitcoin",
+//         "name": "Bitcoin",
+//         "symbol": "BTC",
+//         "rank": "1",
+//         "price_usd": "8088.05",
+//         "price_btc": "1.0",
+//         "24h_volume_usd": "9825660000.0",
+//         "market_cap_usd": "137294244348",
+
+
+//     }, 
+//      {
+//         "id": "siacoin",
+//         "name": "Siacoin",
+//         "symbol": "SC",
+//         "rank": "36",
+//         "price_usd": "0.0144578",
+//         "price_btc": "0.00000178",
+//         "24h_volume_usd": "17730600.0",
+//         "market_cap_usd": "487999542.0",
+//         },
+//      {
+//         "id": "siacoin 3",
+//         "name": "Siacoin 3",
+//         "symbol": "SCD",
+//         "rank": "32",
+//         "price_usd": "0.0144578",
+//         "price_btc": "0.00000178",
+//         "24h_volume_usd": "17730600.0",
+//         "market_cap_usd": "487999542.0",
+//         }
+//     ];
+// function renderData(){
+//   var table = document.getElementById("myTable");
+//   var tableInfo = '';
+//   data.forEach((item, index)=> {
+//     tableInfo += "<tr><td>"+item.id+"</td><td>"+item.name+"</td><td>"+item.symbol+"</td><td>"+item.rank+"</td></tr>"
+//   })
+//   table.innerHTML = tableInfo;
+// }
+
+// renderData();
+// </script>
